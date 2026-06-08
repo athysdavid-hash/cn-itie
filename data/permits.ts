2f3ca64e6f2c27285705bfa4fc764d0521ec358b
unknown <itiecongocellulecom@gmail.com>
2026-06-08 12:34:17 +0200

    fix permits import

---
+++ data/permits.ts
@@ -0,0 +1,127 @@
+const permits = [
+  {
+    name: "Marine III",
+    decree: "Décret n°2005-294 du 18/07/2005",
+    law: "Loi n°10-2006 du 30/03/2006",
+    operator: "SNPC",
+    year: "2005",
+    pdf: "https://www.finances.gouv.cg/sites/default/files/documents/DC%C7crets%20n%C2%AF2005-294.pdf",
+  },
+
+  {
+    name: "Kayo",
+    decree: "Décret n°2006-173 du 14/04/2006",
+    law: "Loi n°07-2008 du 07/04/2008",
+    operator: "Wing Wah",
+    year: "2006",
+    pdf: "https://www.sgg.cg/JO/2006/congo-jo-2006-15.pdf",
+  },
+
+  {
+    name: "Ngoki",
+    decree: "Décret n°2006-427 du 31/07/2006",
+    law: "Loi n°29-06 du 05/10/2006",
+    operator: "SNPC",
+    year: "2006",
+    pdf: "https://www.finances.gouv.cg/sites/default/files/documents/D%20n%C2%AF2006-427.pdf",
+  },
+
+  {
+    name: "Mayombe",
+    decree: "Décret n°2006-426 du 31/07/2006",
+    law: "",
+    operator: "SNPC / PP",
+    year: "2006",
+    pdf: "https://www.sgg.cg/JO/2006/congo-jo-2006-28.pdf",
+  },
+
+  {
+    name: "Marine XIII",
+    decree: "Décret n°2008-54 du 28/03/2008",
+    law: "Loi n°33-2008 du 12/11/2008",
+    operator: "SNPC",
+    year: "2008",
+    pdf: "https://www.finances.gouv.cg/sites/default/files/documents/D%20n%C2%AF2008-54.pdf",
+  },
+
+  {
+    name: "Haute Mer A",
+    decree: "Décret n°2009-228 du 30/07/2009",
+    law: "Loi n°03-2009 du 22/09/2009",
+    operator: "SNPC",
+    year: "2009",
+    pdf: "https://www.sgg.cg/JO/2009/congo-jo-2009.pdf",
+  },
+
+  {
+    name: "Haute Mer B",
+    decree: "Décret n°2013-382 du 19/07/2013",
+    law: "Loi n°22-2014 du 13/06/2014",
+    operator: "SNPC",
+    year: "2013",
+    pdf: "https://www.finances.gouv.cg/sites/default/files/documents/D%202013-382.pdf",
+  },
+
+  {
+    name: "Mopongo",
+    decree: "Décret n°2013-378 du 19/07/2013",
+    law: "",
+    operator: "SNPC",
+    year: "2013",
+    pdf: "https://www.finances.gouv.cg/sites/default/files/documents/D%202013-378.pdf",
+  },
+
+  {
+    name: "Ngolo",
+    decree: "Décret n°2013-379 du 19/07/2013",
+    law: "",
+    operator: "SNPC",
+    year: "2013",
+    pdf: "https://www.finances.gouv.cg/sites/default/files/documents/D%202013-379.pdf",
+  },
+
+  {
+    name: "Marine VI Bis",
+    decree: "Décret n°2015-409 du 22/04/2015",
+    law: "Loi n°6-2017 du 24/02/2017",
+    operator: "SNPC",
+    year: "2015",
+    pdf: "https://economie.gouv.cg/sites/default/files/L%206-2017.pdf",
+  },
+
+  {
+    name: "Marine XII",
+    decree: "Décret n°2006-641 du 30/10/2006",
+    law: "Loi n°1-2010 / Loi n°56-2021",
+    operator: "SNPC / ENI / Lukoil",
+    year: "2006",
+    pdf: "https://www.sgg.cg/JO/2010/congo-jo-2010.pdf",
+  },
+
+  {
+    name: "Marine XXVII",
+    decree: "Décret n°2018-486 du 26/12/2018",
+    law: "Loi n°53-2022",
+    operator: "SNPC / Perenco / AOGC / Petro Congo",
+    year: "2018",
+    pdf: "https://www.sgg.cg/JO/2022/congo-jo-2022.pdf",
+  },
+
+  {
+    name: "Marine XXVIII",
+    decree: "Décret n°2018-487 du 26/12/2018",
+    law: "Loi n°54-2022",
+    operator: "SNPC / Perenco",
+    year: "2018",
+    pdf: "https://www.sgg.cg/JO/2022/congo-jo-2022.pdf",
+  },
+
+  {
+    name: "Marine XX",
+    decree: "Décret n°2019-355 du 30/11/2019",
+    law: "Loi n°61-2022",
+    operator: "TotalEnergies / SNPC / Woodside",
+    year: "2019",
+    pdf: "https://www.sgg.cg/JO/2022/congo-jo-2022.pdf",
+  },
+]
